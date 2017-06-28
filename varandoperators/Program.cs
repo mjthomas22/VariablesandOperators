dc028fb306f2af9c72893f618e6785ceb9764f8f
mjthomas22 <mthomas22@gmail.com>
2017-06-28 09:53:25 -0500

    updated

--- varandoperators/Program.cs
+++ varandoperators/Program.cs
@@ -10,56 +10,87 @@ namespace varandoperators
     {
         static void Main(string[] args)
         {
+            //string donorname = "";
+            //string donoraddress = "";
+            //string donoremail = "";
+            //string fiscalyear = "";
 
-            Console.WriteLine("What is the name on your reservation");
-            string reserveName = Console.ReadLine();
+            //Console.Writeline("donors name");
+            //donorname = console.readline();
 
-            Console.WriteLine("How many people are in your party");
-            int numberOfPeople = int.Parse(Console.ReadLine());
+            //console.writeline("donors address");
+            //donoraddress = console.readline();
 
-            double amountEach = 13.53d;
-            double totalBill = numberOfPeople * amountEach;
-           
-            Console.WriteLine("Reservation Name : {0}",reserveName);
-            Console.WriteLine("Total Bill : ${0}", totalBill);
+            //console.writeline("donors email");
+            //donoremail = console.readline();
 
-            String donorName = "";
-            String donorAddress = "";
-            String donorEmail = "";
-            String fiscalYear = "";
+            //console.writeline("fiscal year");
+            //fiscalyear = console.readline();
 
-            Console.WriteLine("Donors Name");
-            donorName = Console.ReadLine();
+            //console.writeline("how many quarters?");
+            //double quarters = double.parse(console.readline()) * .25;
 
-            Console.WriteLine("Donors Address");
-            donorAddress = Console.ReadLine();
+            //console.writeline("how many ones?");
+            //int ones = int.parse(console.readline());
 
-            Console.WriteLine("Donors Email");
-            donorEmail = Console.ReadLine();
+            //console.writeline("how many fives?");
+            //int fives = int.parse(console.readline()) * 5;
 
-            Console.WriteLine("Fiscal Year");
-            fiscalYear = Console.ReadLine();
+            //console.writeline("how many tens?");
+            //int tens = int.parse(console.readline()) * 10;
 
-            Console.WriteLine("How many quarters?");
-            double quarters = double.Parse(Console.ReadLine()) * .25;
+            //console.writeline("how many twenties?");
+            //int twenties = int.parse(console.readline()) * 20;
 
-            Console.WriteLine("How many ones?");
-            int ones = int.Parse(Console.ReadLine());
+            //console.writeline("how many fifties?");
+            //int fifties = int.parse(console.readline()) * 50;
 
-            Console.WriteLine("How many fives?");
-            int fives = int.Parse(Console.ReadLine()) * 5;
+            //console.writeline("how many hundreds?");
+            //int hundreds = int.parse(console.readline()) * 100;
 
-            Console.WriteLine("How many tens?");
-            int tens = int.Parse(Console.ReadLine()) * 10;
 
-            Console.WriteLine("How many twenties?");
+            //console.writeline("name of donor: " + donorname);
+            //console.writeline("donor's address: " + donoraddress);
+            //console.writeline("donor's email: " + donoremail);
+            //console.writeline("fiscal year: " + fiscalyear);
+            //double donationamount = (quarters + ones + fives + tens + twenties + fifties + hundreds);
+            //console.writeline("donatated amount:$ " + donationamount);
 
-            Console.WriteLine(donorName);
-            Console.WriteLine(donorAddress);
-            Console.WriteLine(donorEmail);
-            Console.WriteLine(fiscalYear);
-            double donationAmount = (quarters + ones + fives + tens);
-            Console.WriteLine("$" + donationAmount);
+            //Types of shirts
+            double superSwim;
+            double theShark;
+            double babyBubble;
+
+            Console.WriteLine("Welcome to the swim team fundraiser!");
+
+            //How many shirts
+            Console.WriteLine("How many Super Swim shirts would you like?");
+            superSwim = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("How many Be the Shark shirts would you like?");
+            theShark = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("How many Baby Bubble shirts would you like?");
+            babyBubble = double.Parse(Console.ReadLine());
+
+            //Reciept
+
+            Console.WriteLine("Thank you for your support!");
+            Console.WriteLine("You purchased " +(superSwim +theShark+ babyBubble) +" Shirts");
+            
+
+            // Total and taxes
+
+            double subTotal = ((superSwim * 15.00) + (theShark * 17.50) + (babyBubble * 4.75));
+            double taxes = (subTotal * .08)+subTotal;
+
+            Console.WriteLine("Subtotal ${0}", subTotal);
+            Console.WriteLine("8% Taxes");
+            Console.WriteLine("Grand Total ${0}", taxes);
+
+
+
+
 
         }
     }
